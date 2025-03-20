6f7930be2137621f09e0b335b914c4d600303c9d
Jakub Katski <kubakajo@poczta.fm>
2025-03-20 21:49:38 +0100

    user profile

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path("create-room/", views.create_room, name='create_room'),
     path("update-room/<str:pk>/", views.update_room, name='update_room'),
     path("delete-room/<str:pk>/", views.delete_room, name='delete_room'),
